443926a0b42af820404bffd0c57b4c6feee16253
ChrisHunt <cljhunt@icloud.com>
2024-03-15 13:23:39 -0400

    new grid layout cause dumb and then started the unit testing

--- Wordle/gridUItesting.c
+++ Wordle/gridUItesting.c
@@ -1,16 +1,30 @@
 #include "stdio.h"
-void graph() {
-	char* first = "H";
+int main(void) {
+	char* first = "H";	//could change "H" to something like gameboard.row[blah][blah]
+	//		first = ####first####
 	char* sec = "e";
 	char* third = "l";
 	char* fourth = "l";
 	char* fifth = "o";
-	for (int i = 0; i < 5; i++)
+	printf("//////////\\\\\\\\\\\\\\\\\\\\\\\n");
+	for (int i = 0; i < 6; i++)
 	{
-		printf("//////////\\\\\\\\\\\\\\\\\\\\\\\n");
+
 		printf("|   |   |   |   |   |\n");
 		printf("| %s | %s | %s | %s | %s |\n", first, sec, third, fourth, fifth);
 		printf("|   |   |   |   |   |\n");
+		if (i < 2)
+		{
+			printf("//////////\\\\\\\\\\\\\\\\\\\\\\\n");
+		}if (i == 2)
+		{
+			printf("|||||||||||||||||||||\n");
+		}
+		if (i > 2)
+		{
+			printf("\\\\\\\\\\\\\\\\\\\\\\\//////////\n");
+		}
 	}
-	printf("\\\\\\\\\\\\\\\\\\\\\\\//////////\n");
+
+	return 0;
 }
---
+++ WorldeTesting/WorldeTesting.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace WorldeTesting
+{
+	TEST_CLASS(WorldeTesting)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
